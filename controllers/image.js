bb41eefbefa64c7430a4f0faf4eb6314e2d6b393
viettu2k <doanviettu2k@gmail.com>
2023-09-18 22:37:23 +0700

    adding docker file

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,31 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
-    apiKey: "adba52bc5b5244ae8b76984fa7836587",
+  apiKey: 'YOUR_API_KEY_HERE',
 });
 
 const handleApiCall = (req, res) => {
-    app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => res.status(400).json('unable to work with API'))
-}
+  app.models
+    .predict('face-detection', req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
 
 const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-}
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
-    handleImage,
-    handleApiCall
-}
+  handleImage,
+  handleApiCall,
+};
